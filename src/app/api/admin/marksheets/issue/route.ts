e7af006d15a00d9e0dd5022f6028ef3cc92841c6
Jason3105 <jasongons312005@gmail.com>
2026-06-15 18:47:57 +0530

    dotenv package and dynamic page serving

--- src/app/api/admin/marksheets/issue/route.ts
+++ src/app/api/admin/marksheets/issue/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { NextResponse } from 'next/server'
 import { PDFDocument, rgb, StandardFonts } from 'pdf-lib'
 import { uploadToS3 } from '@/lib/s3'
